d2c5acb2ac05227d6775f935d5cd53bff297239c
jeyofdev <jgregoire.dev@gmail.com>
2024-05-03 16:01:57 +0200

    feat : create component header

--- src/app/modules/activity/activity.module.ts
+++ src/app/modules/activity/activity.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ActivityRoutingModule } from './activity-routing.module';
 import { ActivityHomeComponent } from './pages/activity-home/activity-home.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    ActivityHomeComponent
-  ],
-  imports: [
-    CommonModule,
-    ActivityRoutingModule
-  ]
+  declarations: [ActivityHomeComponent],
+  imports: [CommonModule, ActivityRoutingModule, SharedModule],
 })
-export class ActivityModule { }
+export class ActivityModule {}
