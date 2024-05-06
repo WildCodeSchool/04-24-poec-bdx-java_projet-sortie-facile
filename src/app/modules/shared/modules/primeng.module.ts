534633b40d89bc0d43c508ca536f5996e6a5c2a0
Laurore83 <laurore83@yahoo.fr>
2024-05-06 12:49:58 +0200

    modal component created

--- src/app/modules/shared/modules/primeng.module.ts
+++ src/app/modules/shared/modules/primeng.module.ts
@@ -3,8 +3,16 @@ import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
-
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
 @NgModule({
-  exports: [ButtonModule, MenubarModule, InputTextModule, PasswordModule],
+  exports: [
+    ButtonModule,
+    MenubarModule,
+    InputTextModule,
+    PasswordModule,
+    ConfirmDialogModule,
+    ToastModule,
+  ],
 })
 export class PrimengModule {}
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,15 +4,17 @@ import { PrimengModule } from './modules/primeng.module';
 import { FormsModule } from '@angular/forms';
 import { TextFieldComponent } from './components/form/text-field/text-field.component';
 import { PasswordFieldComponent } from './components/form/password-field/password-field.component';
+import { ModalComponent } from './components/modal/modal/modal.component';
 
 @NgModule({
-  declarations: [TextFieldComponent, PasswordFieldComponent],
+  declarations: [TextFieldComponent, PasswordFieldComponent, ModalComponent],
   imports: [CommonModule, PrimengModule, FormsModule],
   exports: [
     PrimengModule,
     FormsModule,
     TextFieldComponent,
     PasswordFieldComponent,
+    ModalComponent,
   ],
 })
 export class SharedModule {}
