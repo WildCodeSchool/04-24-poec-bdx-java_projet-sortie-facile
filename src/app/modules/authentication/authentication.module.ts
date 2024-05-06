f01fd6fe825f2f8b05c1a954264934ff301de9d1
jeyofdev <jgregoire.dev@gmail.com>
2024-05-06 09:43:15 +0200

    feat : init page login and route auth/login

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { AuthLoginComponent } from './pages/auth-login/auth-login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthLoginComponent
+  ],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
---
+++ src/app/modules/authentication/pages/auth-login/auth-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-login',
+  templateUrl: './auth-login.component.html',
+  styleUrl: './auth-login.component.scss'
+})
+export class AuthLoginComponent {
+
+}
