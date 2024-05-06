8cde9b03d254bc7ae2c791298f02e76d41f6e203
Laurore83 <laurore83@yahoo.fr>
2024-05-06 16:47:56 +0200

    sat

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -8,7 +8,6 @@ import { FieldErrorComponent } from './components/form/field-error/field-error.c
 import { ModalComponent } from './components/modal/modal/modal.component';
 import { InputDateComponent } from './components/date/input-date/input-date.component';
 import { InputHourComponent } from './components/date/input-hour/input-hour.component';
-import { SelectCityComponent } from './components/select/select-city/select-city.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import { SelectCityComponent } from './components/select/select-city/select-city
     ModalComponent,
     InputDateComponent,
     InputHourComponent,
-    SelectCityComponent,
   ],
   imports: [CommonModule, PrimengModule, FormsModule],
   exports: [
@@ -30,7 +28,6 @@ import { SelectCityComponent } from './components/select/select-city/select-city
     ModalComponent,
     InputDateComponent,
     InputHourComponent,
-    SelectCityComponent,
   ],
 })
 export class SharedModule {}
