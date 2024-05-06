741f38bf8df2a77722bb5d9aa8b81ac4bba23516
yoyodes1000 <yoyodes1000@gmail.com>
2024-05-06 14:02:09 +0200

    commit before pull dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { SharedModule } from './modules/shared/shared.module';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
-import { CreateAccountComponent } from './modules/authentication/components/feature/create-account/create-account.component';
+import { CreateAccountComponent } from './modules/authentication/pages/create-account/create-account.component';
 
 @NgModule({
   declarations: [AppComponent, PagenotfoundComponent, HeaderComponent, CreateAccountComponent],
