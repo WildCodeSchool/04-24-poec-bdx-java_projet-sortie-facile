50f7fa590c81affb85e03ff320a9d82c4d1993a7
jeyofdev <jgregoire.dev@gmail.com>
2024-05-07 15:04:23 +0200

    style : page landing/home

---
+++ src/app/modules/landing/pages/landing-home/landing-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-home',
+  templateUrl: './landing-home.component.html',
+  styleUrl: './landing-home.component.scss',
+})
+export class LandingHomeComponent {}
