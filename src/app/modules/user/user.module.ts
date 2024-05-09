12a78ecd7c195c92ec9f1bb292287b5a30986348
jeyofdev <jgregoire.dev@gmail.com>
2024-05-09 12:28:45 +0200

    feat : create component layout-header in module user

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -6,9 +6,10 @@ import { UserHomeComponent } from './pages/user-home/user-home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AccountLayoutComponent } from './components/ui/account-layout/account-layout.component';
 import { AccountGeneralManagementComponent } from './components/feature/account-general-management/account-general-management.component';
+import { LayoutHeaderComponent } from './components/ui/layout/layout-header/layout-header.component';
 
 @NgModule({
-  declarations: [UserHomeComponent, AccountLayoutComponent, AccountGeneralManagementComponent],
+  declarations: [UserHomeComponent, AccountLayoutComponent, AccountGeneralManagementComponent, LayoutHeaderComponent],
   imports: [CommonModule, UserRoutingModule, SharedModule],
 })
 export class UserModule {}
