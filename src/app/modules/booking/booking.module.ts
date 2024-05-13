3a9ba8b7427238ec54158f48cfd79da88fa402bc
Laurore83 <laurore83@yahoo.fr>
2024-05-13 18:34:51 +0200

    post new resservation done

--- src/app/modules/booking/booking.module.ts
+++ src/app/modules/booking/booking.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { BookingHomeComponent } from './pages/booking-home/booking-home.component';
+import { SharedModule } from 'primeng/api';
 
 @NgModule({
 	declarations: [BookingHomeComponent],
-	imports: [CommonModule, BookingRoutingModule],
+	imports: [CommonModule, BookingRoutingModule, SharedModule],
 })
 export class BookingModule {}
--- src/app/modules/shared/services/booking/booking.service.ts
+++ src/app/modules/shared/services/booking/booking.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { reservation } from '@shared/models/types/reservation.type';
 import { reservations } from '@shared/models/types/reservations.type';
-import { Observable, map } from 'rxjs';
+import { Observable, catchError, map, switchMap, tap } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
@@ -14,5 +14,31 @@ export class BookingService {
 		return this.http
 			.get<reservations>('http://localhost:3000/reservation')
 			.pipe(map((response: reservations) => response));
+	}
+
+	postNewReservation$(newReservation: reservation): Observable<reservation> {
+		return this.http
+			.get<reservation[]>('http://localhost:3000/reservation')
+			.pipe(
+				switchMap(reservations => {
+					const nextId =
+						reservations.length > 0
+							? Number(reservations[reservations.length - 1].id) + 1
+							: 1;
+					newReservation.id = String(nextId);
+
+					return this.http.post<reservation>(
+						'http://localhost:3000/reservation',
+						newReservation,
+					);
+				}),
+				tap(data => {
+					console.log('POST Request is successful ', data);
+				}),
+				catchError(error => {
+					console.log('Error', error);
+					throw error;
+				}),
+			);
 	}
 }
