f247c874e1ebdb1c4ec7d9a48dec2ce8ce2efc3c
Laurore83 <laurore83@yahoo.fr>
2024-05-17 10:45:43 +0200

    fixed update activity

--- src/app/modules/shared/components/modal/modal-confirm-update-activity/modal-confirm-update-activity.component.ts
+++ src/app/modules/shared/components/modal/modal-confirm-update-activity/modal-confirm-update-activity.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Activity } from '@shared/models/types/activity.type';
+import { ActivityService } from '@shared/services/activity.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
-  selector: 'app-modal-confirm-update-activity',
-  templateUrl: './modal-confirm-update-activity.component.html',
-  styleUrl: './modal-confirm-update-activity.component.scss'
+	selector: 'app-modal-confirm-update-activity',
+	templateUrl: './modal-confirm-update-activity.component.html',
+	styleUrl: './modal-confirm-update-activity.component.scss',
+	providers: [ConfirmationService, MessageService],
 })
 export class ModalConfirmUpdateActivityComponent {
+	@Input() myForm!: NgForm;
+	@Input() activityId!: string;
+	constructor(
+		private confirmationService: ConfirmationService,
+		private messageService: MessageService,
+		private activityService: ActivityService,
+		private router: Router,
+	) {}
 
+	onSubmit() {
+		if (this.myForm && this.myForm.valid) {
+			this.confirmationService.confirm({
+				header: 'Confirmation',
+				message: 'Confirmer la création de votre activité',
+				accept: () => {
+					const updatedData = this.myForm.value;
+					this.activityService
+						.updateActivity$(this.activityId, updatedData)
+						.subscribe(
+							(activity: Activity) => {
+								this.messageService.add({
+									severity: 'info',
+									summary: 'Bravo',
+									detail: 'Votre activité a bien été créée',
+									life: 3000,
+								});
+								setTimeout(() => {
+									this.router.navigate(['/activity/details', activity?.id]);
+								}, 3000);
+							},
+							error => {
+								console.error('Error creating activity:', error);
+								this.messageService.add({
+									severity: 'error',
+									summary: 'Erreur',
+									detail:
+										"Une erreur s'est produite lors de la création de l'activité",
+									life: 3000,
+								});
+							},
+						);
+				},
+				reject: () => {
+					this.messageService.add({
+						severity: 'error',
+						summary: 'Abandonné',
+						detail: 'Création abandonnée',
+						life: 3000,
+					});
+				},
+			});
+		} else {
+			this.messageService.add({
+				severity: 'error',
+				summary: 'Formulaire invalide',
+				detail: 'Veuillez remplir les champs obligatoires',
+				life: 3000,
+			});
+		}
+	}
 }
