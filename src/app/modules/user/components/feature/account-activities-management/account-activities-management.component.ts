3b0fb9fb25ddb7c23df544ba8be2f8c25861b19d
jeyofdev <jgregoire.dev@gmail.com>
2024-05-19 19:54:45 +0200

    refacto : modals

--- src/app/modules/user/components/feature/account-activities-management/account-activities-management.component.ts
+++ src/app/modules/user/components/feature/account-activities-management/account-activities-management.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Activity } from '@shared/models/types/activity.type';
 import { UserAuthPrimaryDatas } from '@shared/models/types/user-list-response-api.type';
 import { ActivityService } from '@shared/services/activity.service';
 import { AuthService } from '@shared/services/auth.service';
-import { tap } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
 	selector: 'app-account-activities-management',
 	templateUrl: './account-activities-management.component.html',
 	styleUrl: './account-activities-management.component.scss',
 })
-export class AccountActivitiesManagementComponent implements OnInit {
+export class AccountActivitiesManagementComponent implements OnInit, OnDestroy {
 	@Input() avatarSrc!: string;
 	@Input() avatarAlt!: string;
 	@Input() pageTitle!: string;
@@ -19,6 +19,7 @@ export class AccountActivitiesManagementComponent implements OnInit {
 	activityParticipateList!: Activity[];
 
 	connectedUser!: UserAuthPrimaryDatas;
+	private _subscription: Subscription = new Subscription();
 
 	constructor(
 		private _authService: AuthService,
@@ -36,15 +37,19 @@ export class AccountActivitiesManagementComponent implements OnInit {
 			)
 			.subscribe();
 
-		// TODO
-		// create service reservation by user
-		this._activityService
-			.getActivityListByCreatedUser$(10)
-			.pipe(
-				tap(activities => {
-					this.activityByCreatedUserList = activities;
-				}),
-			)
-			.subscribe();
+		this._subscription.add(
+			this._activityService
+				.getActivityListByCreatedUser$(10)
+				.pipe(
+					tap(activities => {
+						this.activityByCreatedUserList = activities;
+					}),
+				)
+				.subscribe(),
+		);
+	}
+
+	ngOnDestroy(): void {
+		this._subscription.unsubscribe();
 	}
 }
