16d09088a4875bf4ee753c7689c332ab5e315055
jeyofdev <jgregoire.dev@gmail.com>
2024-05-20 22:48:14 +0200

    feat : add methds for update password

--- src/app/modules/user/components/ui/account-form/account-password-form/account-password-form.component.ts
+++ src/app/modules/user/components/ui/account-form/account-password-form/account-password-form.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserPasswordFormDatas } from '@shared/models/classes/user-password-form-datas.class';
+import { UserAuthPrimaryDatas } from '@shared/models/types/user-list-response-api.type';
 import { AuthService } from '@shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-account-password-form',
 	templateUrl: './account-password-form.component.html',
 	styleUrl: './account-password-form.component.scss',
 })
-export class AccountPasswordFormComponent {
+export class AccountPasswordFormComponent implements OnDestroy {
+	@Input() connectedUser!: UserAuthPrimaryDatas;
+	private _subscription: Subscription = new Subscription();
+
 	accountPasswordFormDatas: UserPasswordFormDatas = new UserPasswordFormDatas(
 		'',
 		'',
 		'',
 	);
 
+	constructor(private _authService: AuthService) {}
+
 	onSubmit(form: NgForm): void {
-		console.log(form.value);
+		this._subscription.add(
+			this._authService
+				.updatePassword(
+					this.connectedUser,
+					this.accountPasswordFormDatas.oldPassword,
+					this.accountPasswordFormDatas.newPassword,
+				)
+				.subscribe(),
+		);
+	}
+
+	ngOnDestroy(): void {
+		this._subscription.unsubscribe();
 	}
 }
