4d7af9c2b0004a96154aedc7b137346afda1560f
jeyofdev <jgregoire.dev@gmail.com>
2024-05-23 21:31:05 +0200

    feat : hide or show link create activity when user is connected or not

--- src/app/modules/activity/components/feature/activity-list-management/activity-list-management.component.ts
+++ src/app/modules/activity/components/feature/activity-list-management/activity-list-management.component.ts
@@ -31,9 +31,9 @@ export class ActivityListManagementComponent implements OnInit, OnChanges {
 	totalRecords: number = 0;
 
 	rowsPerPageOptions = [
-		{ label: '10', value: 10 },
-		{ label: '20', value: 20 },
-		{ label: '30', value: 30 },
+		{ label: '1', value: 1 },
+		{ label: '2', value: 2 },
+		{ label: '3', value: 3 },
 	];
 
 	constructor(
