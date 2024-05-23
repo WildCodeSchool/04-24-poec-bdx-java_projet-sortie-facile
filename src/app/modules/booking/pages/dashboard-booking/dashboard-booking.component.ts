59d0bb62f61b3b84e92a4379d88b342c76e328df
jeyofdev <jgregoire.dev@gmail.com>
2024-05-23 17:51:50 +0200

    fix conflicts

--- src/app/modules/booking/pages/dashboard-booking/dashboard-booking.component.ts
+++ src/app/modules/booking/pages/dashboard-booking/dashboard-booking.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard-booking',
-  templateUrl: './dashboard-booking.component.html',
-  styleUrl: './dashboard-booking.component.scss'
+	selector: 'app-dashboard-booking',
+	templateUrl: './dashboard-booking.component.html',
+	styleUrl: './dashboard-booking.component.scss',
 })
-export class DashboardBookingComponent {
-
-}
+export class DashboardBookingComponent {}
