195a91ef154fe11da6b6d07260e415fdf6624e3a
jeyofdev <jgregoire.dev@gmail.com>
2024-05-26 10:59:02 +0200

    reafacto : create sub modules pipe, directive and shared-form

--- src/app/modules/activity/activity-routing.module.ts
+++ src/app/modules/activity/activity-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ActivityDetailsComponent } from './pages/activity-details/activity-details.component';
-import { UpdateActivityComponent } from './pages/update-activity/update-activity.component';
-import { ActivityListComponent } from './pages/activity-list/activity-list.component';
-import { ActivityCreateComponent } from './pages/activity-create/activity-create.component';
+import { ActivityDetailsComponent } from '@activity/pages/activity-details/activity-details.component';
+import { UpdateActivityComponent } from '@activity/pages/update-activity/update-activity.component';
+import { ActivityListComponent } from '@activity/pages/activity-list/activity-list.component';
+import { ActivityCreateComponent } from '@activity/pages/activity-create/activity-create.component';
 
 const routes: Routes = [
 	{ path: 'create', component: ActivityCreateComponent },
--- src/app/modules/authentication/authentication-routing.module.ts
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthLoginComponent } from '@authentication/pages/auth-login/auth-login.component';
-import { AuthRegisterComponent } from './pages/auth-register/auth-register.component';
+import { AuthRegisterComponent } from '@authentication/pages/auth-register/auth-register.component';
 
 const routes: Routes = [
 	{
