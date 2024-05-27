abc89e6b214ab09de559638437787f7ec5fff5ac
Laurore83 <laurore83@yahoo.fr>
2024-05-27 20:58:50 +0200

    tag green user connected in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthUserPrimaryDatas } from '@shared/models/classes/auth-user/auth-user-primary-datas.class';
 import { AuthService } from '@shared/services/auth.service';
 import { HeaderService } from '@shared/services/header.service';
 import { MenuItem } from 'primeng/api';
@@ -12,7 +13,8 @@ import { Observable } from 'rxjs';
 export class HeaderComponent implements OnInit {
 	profileItems$!: Observable<MenuItem[]>;
 	items$!: Observable<MenuItem[]>;
-
+	isUserLoggedIn: boolean = true;
+	@Input() connectedUser!: AuthUserPrimaryDatas;
 	constructor(
 		private _authService: AuthService,
 		private _headerService: HeaderService,
