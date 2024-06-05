077aa4e0d17fe88a5d16166d1eda6ddd3adf9408
jeyofdev <jgregoire.dev@gmail.com>
2024-06-06 00:28:33 +0200

    refacto : create service auth response

---
+++ src/app/modules/shared/services/auth-response.service.ts
@@ -0,0 +1,32 @@
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AuthResponseService {
+	private _httpErrorSubject$: BehaviorSubject<HttpErrorResponse> =
+		new BehaviorSubject(new HttpErrorResponse({}));
+
+	private _httpSuccessSubject$: BehaviorSubject<HttpResponse<unknown>> =
+		new BehaviorSubject(new HttpResponse({}));
+
+	getHttpErrorSubject$(): Observable<HttpErrorResponse> {
+		return this._httpErrorSubject$.asObservable();
+	}
+
+	setHttpErrorSubject$(error: HttpErrorResponse): void {
+		this._httpSuccessSubject$.next(new HttpResponse({}));
+		this._httpErrorSubject$.next(error);
+	}
+
+	getHttpSuccessSubject$(): Observable<HttpResponse<unknown>> {
+		return this._httpSuccessSubject$.asObservable();
+	}
+
+	setHttpSuccessSubject$(success: HttpResponse<unknown>): void {
+		this._httpErrorSubject$.next(new HttpErrorResponse({}));
+		this._httpSuccessSubject$.next(success);
+	}
+}
