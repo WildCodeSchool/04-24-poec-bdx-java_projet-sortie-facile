658c3d36d8c589f1a48cf3313181afe8ae633646
Laurore83 <laurore83@yahoo.fr>
2024-06-08 17:39:53 +0200

    badge notif if is admin add

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthUserPrimaryDatas } from '@shared/models/classes/auth-user/auth-user-primary-datas.class';
+import { FullBookingRouteEnum } from '@shared/models/enums/routes/full-routes';
+import { UserRoleEnum } from '@shared/models/enums/user-role.enum';
 import { AuthService } from '@shared/services/auth.service';
 import { HeaderService } from '@shared/services/header.service';
 import { MenuItem } from 'primeng/api';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Component({
 	selector: 'app-header',
@@ -10,10 +13,11 @@ import { Observable } from 'rxjs';
 	styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
+	fullBookingRouteEnum = FullBookingRouteEnum;
 	profileItems$!: Observable<MenuItem[]>;
 	items$!: Observable<MenuItem[]>;
 	isUserLoggedIn: boolean = false;
-
+	isAdmin: boolean = false;
 	constructor(
 		private _authService: AuthService,
 		private _headerService: HeaderService,
@@ -23,5 +27,13 @@ export class HeaderComponent implements OnInit {
 		this._authService.checkIfUserIsConnectedAndNotifyLoggedInStatus();
 		this.items$ = this._headerService.getPrimaryItems$();
 		this.profileItems$ = this._headerService.getIsLoggedInItems$();
+		this._authService
+			.getConnectedUserObservable()
+			.pipe(
+				map((user: AuthUserPrimaryDatas) => user.role === UserRoleEnum.ADMIN),
+			)
+			.subscribe((isAdmin: boolean) => {
+				this.isAdmin = isAdmin;
+			});
 	}
 }
