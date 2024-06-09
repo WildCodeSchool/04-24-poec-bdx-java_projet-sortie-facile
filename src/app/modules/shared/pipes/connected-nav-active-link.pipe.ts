21fe1a5ce9ccea0df0e9e821f7b16ab3cee37531
jeyofdev <jgregoire.dev@gmail.com>
2024-06-09 17:49:28 +0200

    refacto : pipe connected-nav-active-link.pipe

--- src/app/modules/shared/pipes/connected-nav-active-link.pipe.ts
+++ src/app/modules/shared/pipes/connected-nav-active-link.pipe.ts
@@ -3,13 +3,14 @@ import { ActivatedRoute } from '@angular/router';
 import { LayoutLink } from '@shared/models/types/utils/layout-link.type';
 
 @Pipe({
-	name: 'accountNavActiveLink',
+	name: 'connectedNavActiveLink',
 })
-export class AccountNavActiveLinkPipe implements PipeTransform {
+export class ConnectedNavActiveLinkPipe implements PipeTransform {
 	constructor(private _activatedRoute: ActivatedRoute) {}
 
-	transform(items: LayoutLink[]): LayoutLink[] {
-		const url: string = '/user/' + this._activatedRoute.snapshot.url[0].path;
+	transform(items: LayoutLink[], primaryRoute: string): LayoutLink[] {
+		const url: string =
+			`/${primaryRoute}/` + this._activatedRoute.snapshot.url[0].path;
 
 		return items.map(item => ({ ...item, active: url === item.path }));
 	}
