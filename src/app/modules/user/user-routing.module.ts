749039be1dde2d091fd8920d713922eaf758570a
jeyofdev <jgregoire.dev@gmail.com>
2024-06-20 22:32:24 +0200

    feat : connect front-back profile notifs

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -5,7 +5,7 @@ import { UserHomeComponent } from '@user/pages/user-home/user-home.component';
 import { UserProfileComponent } from '@user/pages/user-profile/user-profile.component';
 import { UserPasswordComponent } from '@user/pages/user-password/user-password.component';
 import { UserCenterOfInterestComponent } from '@user/pages/user-center-of-interest/user-center-of-interest.component';
-// import { UserNotificationComponent } from '@user/pages/user-notification/user-notification.component';
+import { UserNotificationComponent } from '@user/pages/user-notification/user-notification.component';
 import { UserActivitiesComponent } from '@user/pages/user-activities/user-activities.component';
 import { UserCalendarComponent } from '@user/pages/user-calendar/user-calendar.component';
 import { UserRouteEnum } from '@shared/models/enums/routes/route.enum';
@@ -32,11 +32,11 @@ const routes: Routes = [
 		component: UserCenterOfInterestComponent,
 		canActivate: [IsConnectedGuard],
 	},
-	// {
-	// 	path: UserRouteEnum.NOTIFICATION,
-	// 	component: UserNotificationComponent,
-	// 	canActivate: [IsConnectedGuard],
-	// },
+	{
+		path: UserRouteEnum.NOTIFICATION,
+		component: UserNotificationComponent,
+		canActivate: [IsConnectedGuard],
+	},
 	{
 		path: UserRouteEnum.ACTIVITY,
 		component: UserActivitiesComponent,
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -9,7 +9,7 @@ import { UserPasswordComponent } from '@user/pages/user-password/user-password.c
 import { UserActivitiesComponent } from '@user/pages/user-activities/user-activities.component';
 import { UserCalendarComponent } from '@user/pages/user-calendar/user-calendar.component';
 import { UserCenterOfInterestComponent } from '@user/pages/user-center-of-interest/user-center-of-interest.component';
-// import { UserNotificationComponent } from '@user/pages/user-notification/user-notification.component';
+import { UserNotificationComponent } from '@user/pages/user-notification/user-notification.component';
 import { SharedModule } from '@shared/shared.module';
 import { AccountProfileManagementComponent } from '@user/components/feature/account-profile-management/account-profile-management.component';
 import { AccountCardInfoComponent } from '@user/components/ui/account-card-info/account-card-info.component';
@@ -24,7 +24,7 @@ import { AccountCalendarManagementComponent } from './components/feature/account
 import { AccountCalendarComponent } from './components/ui/account-calendar/account-calendar.component';
 import { AccountGeneralFormComponent } from './components/ui/account-form/account-general-form/account-general-form.component';
 import { AccountGeneralManagementComponent } from './components/feature/account-general-management/account-general-management.component';
-// import { AccountNotifManagementComponent } from './components/feature/account-notif-management/account-notif-management.component';
+import { AccountNotifManagementComponent } from './components/feature/account-notif-management/account-notif-management.component';
 import { UserBookingComponent } from './pages/user-booking/user-booking.component';
 import { AccountBookingManagementComponent } from './components/feature/account-booking-management/account-booking-management.component';
 
@@ -38,7 +38,7 @@ import { AccountBookingManagementComponent } from './components/feature/account-
 		UserActivitiesComponent,
 		UserCalendarComponent,
 		UserCenterOfInterestComponent,
-		// UserNotificationComponent,
+		UserNotificationComponent,
 		AccountProfileManagementComponent,
 		AccountCardInfoComponent,
 		AccountActivitiesManagementComponent,
@@ -50,7 +50,7 @@ import { AccountBookingManagementComponent } from './components/feature/account-
 		AccountCalendarManagementComponent,
 		AccountCalendarComponent,
 		AccountGeneralFormComponent,
-		// AccountNotifManagementComponent,
+		AccountNotifManagementComponent,
 		UserBookingComponent,
 		AccountBookingManagementComponent,
 	],
