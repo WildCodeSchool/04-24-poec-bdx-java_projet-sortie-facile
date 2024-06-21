255201954cd4efcb0b487d198359a97b9e4990f6
jeyofdev <jgregoire.dev@gmail.com>
2024-06-21 21:33:18 +0200

    feat : update general profile

---
+++ src/app/modules/shared/models/classes/user-details/update-user-profile-input.class.ts
@@ -0,0 +1,36 @@
+import { UserGenderEnum } from '@shared/models/enums/user-genre.enum';
+
+export class UpdateUserProfileInput {
+	firstname: string;
+	lastname: string;
+	streetNumber: string;
+	street: string;
+	postalCode: number;
+	description: string;
+	avatar: string;
+	phone: string;
+	dateOfBirth: string;
+
+	constructor(
+		firstname: string,
+		lastname: string,
+		streetNumber: string,
+		street: string,
+		postalCode: number,
+		description: string,
+		avatar: string,
+		phone: string,
+		dateOfBirth: string,
+	) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.streetNumber = streetNumber;
+		this.street = street;
+		this.postalCode = postalCode;
+
+		this.description = description;
+		this.avatar = avatar;
+		this.phone = phone;
+		this.dateOfBirth = dateOfBirth;
+	}
+}
--- src/app/modules/shared/services/user.service.ts
+++ src/app/modules/shared/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NewProfileInput } from '@shared/models/classes/user-details/new-profil-input.class';
+import { UpdateUserProfileInput } from '@shared/models/classes/user-details/update-user-profile-input.class';
 import { UserGeneralForm } from '@shared/models/classes/user-details/user-details-general-form.class';
 import { UserProfilePatch } from '@shared/models/classes/user-details/user-details-patch.class';
 import { UserProfilePersonalInfosForm } from '@shared/models/classes/user-details/user-details-personal-info-form.class';
@@ -33,13 +34,33 @@ export class UserService {
 		);
 	}
 
-	putUserInfo$(
-		userId: number,
-		userInfos: UserProfilePersonalInfosForm | UserGeneralForm,
-	): Observable<UserProfile> {
-		return this._httpClient.put<UserProfile>(
-			`${this._BASE_URL}/${userId}`,
-			userInfos,
+	// putUserInfo$(
+	// 	userId: number,
+	// 	userInfos: UserProfilePersonalInfosForm | UserGeneralForm,
+	// ): Observable<UserProfile> {
+	// 	return this._httpClient.put<UserProfile>(
+	// 		`${this._BASE_URL}/${userId}`,
+	// 		userInfos,
+	// 	);
+	// }
+
+	putUserInfo$(userId: number, userInfos: any): Observable<any> {
+		const profileRequestBody: UpdateUserProfileInput =
+			new UpdateUserProfileInput(
+				userInfos.firstname,
+				userInfos.lastname,
+				userInfos.streetNumber,
+				userInfos.street,
+				userInfos.postalCode,
+				userInfos.description,
+				userInfos.avatar,
+				userInfos.phone,
+				userInfos.dateOfBirth,
+			);
+
+		return this._httpClient.put<any>(
+			`${this._BASE_URL}/update/${userId}`,
+			profileRequestBody,
 		);
 	}
 
